1fa64211aefc172ca398d5799c9edae9eabce0a9
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-07-22 19:36:56 +0500

    team images aspect ratio set

--- src/components/page/Team.tsx
+++ src/components/page/Team.tsx
@@ -40,8 +40,9 @@ export default function Team() {
                         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-4 md:mt-6">
                             {teamMembers.map((member, index) => {
                                 return (
-                                    <div key={index} className="relative bg-[#f9f6f1] overflow-hidden">
-                                        <img loading="lazy" src={member.image} alt={member.name} className="w-full h-full object-cover" />
+                                    <div key={index} className="relative bg-[#f9f6f1] overflow-hidden aspect-square">
+                                        <div><img loading="lazy" src={member.image} alt={member.name}
+                                                className="w-full h-full object-cover aspect-square"/></div>
                                         <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-br from-[rgba(4,11,16,0.64)] to-[rgba(4,11,16,0.17)]"></div>
 
                                         {Boolean(toggledMembersInfo.includes(index)) && <div
